a4f82ab284498d5aa202956e131214d998b34396
bop192 <124259758+bop192@users.noreply.github.com>
2023-02-18 17:06:51 +0700

    Add files via upload

---
+++ Person.java
@@ -0,0 +1,14 @@
+public class Person {
+    private String name;
+    private int age;
+    private String adress;
+    public Person(String name, int age, String adress) {
+       this.name = name;
+       this.age = age;
+       this.adress = adress;
+   }
+       System.out.println("\tTen: " + name);
+       System.out.printn("\tTuoi:"+ age);
+       System.out.printn("\tDiachi:" + adress);
+   }
+}
