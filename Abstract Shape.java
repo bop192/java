25c5d0075dc1eef8f3ecd004676dfd53c84e5baf
bop192 <124259758+bop192@users.noreply.github.com>
2023-03-10 16:03:41 +0700

    Add files via upload

---
+++ Abstract Shape.java
@@ -0,0 +1,8 @@
+public abstract class Shape {
+  // Phuong thuc truu tuong tinh dien tich    
+    public abstract double area() {
+
+    // Phuong thuc hien thi thong tin
+    public String toString()  {
+    }    
+}  
