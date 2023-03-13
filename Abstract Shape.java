cbe80e1c9a5a58ab26e6ee011975af3727ba7d92
bop192 <124259758+bop192@users.noreply.github.com>
2023-03-13 07:35:11 +0700

    Delete Abstract Shape.java

--- Abstract Shape.java
+++
@@ -1,8 +0,0 @@
-public abstract class Shape {
-  // Phuong thuc truu tuong tinh dien tich    
-    public abstract double area() {
-
-    // Phuong thuc hien thi thong tin
-    public String toString()  {
-    }    
-}  
